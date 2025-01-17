1628af67e7b0cdcb80a684e3a3f078ef3a187b2d
Pedro-B-Freitag <pedrobosini09@icloud.com>
2025-01-16 23:27:58 -0300

    .

--- Filmes/src/main/resources/static/js/index.js
+++ Filmes/src/main/resources/static/js/index.js
@@ -40,9 +40,6 @@ function pesquisarFilme() {
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         txtValue = a.textContent || a.innerText;
-        if(filmePoster.toString() === "N/A"){
-            li[i].style.display = "none";
-        }
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
